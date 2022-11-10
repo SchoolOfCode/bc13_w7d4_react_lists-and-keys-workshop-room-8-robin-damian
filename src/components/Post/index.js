4a5f948321e624be8857f7a0cf3e37c70282e044
Robin <lawn.tee.robin@gmail.com>
2022-11-10 16:52:31 +0000

    nearly done with task 5

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-function Post() {
-  return <article></article>;
+function Post({ title, date, author, text, highlights, image }) {
+  return (
+    <article>
+      <h1>{title}</h1>
+      <h2>{date}</h2>
+      <h2>{author}</h2>
+      <p>{text}</p>
+      <h2>{highlights}</h2>
+      <img src={image.link} alt={image.alt} />
+    </article>
+  );
 }
 
 export default Post;
--- src/libs/data.js
+++ src/libs/data.js
@@ -1,4 +1,4 @@
-import { LoremIpsum } from 'lorem-ipsum';
+import { LoremIpsum } from "lorem-ipsum";
 
 const lorem = new LoremIpsum({
   sentencesPerParagraph: {
@@ -13,24 +13,24 @@ const lorem = new LoremIpsum({
 
 const postTemplate = {
   postId: 0,
-  title: 'a title',
-  date: 'DD/MM/YY',
-  author: 'author',
+  title: "a title",
+  date: "DD/MM/YY",
+  author: "author",
   text: lorem.generateParagraphs(7),
   highlights: [
     lorem.generateWords(3),
     lorem.generateWords(3),
     lorem.generateWords(3),
   ],
-  image: { link: 'link', alt: 'alt' },
+  image: { link: "link", alt: "alt" },
 };
 
 const samplePosts = [
   {
     postId: 1,
-    title: 'One Of My Favourites',
-    date: '12/10/20',
-    author: 'Bobby Nest',
+    title: "One Of My Favourites",
+    date: "12/10/20",
+    author: "Bobby Nest",
     text: lorem.generateParagraphs(7),
     highlights: [
       lorem.generateWords(3),
@@ -38,16 +38,15 @@ const samplePosts = [
       lorem.generateWords(3),
     ],
     image: {
-      link:
-        'https://i.pinimg.com/originals/71/a0/5f/71a05f47b8f449fc231881416bf4bc66.jpg',
-      alt: 'A blue-grey pigeon turns to face the camera.',
+      link: "https://i.pinimg.com/originals/71/a0/5f/71a05f47b8f449fc231881416bf4bc66.jpg",
+      alt: "A blue-grey pigeon turns to face the camera.",
     },
   },
   {
     postId: 2,
-    title: 'Look At This Marvellous Pigeon',
-    date: '14/10/20',
-    author: 'Lucy Wing',
+    title: "Look At This Marvellous Pigeon",
+    date: "14/10/20",
+    author: "Lucy Wing",
     text: lorem.generateParagraphs(7),
     highlights: [
       lorem.generateWords(3),
@@ -55,15 +54,15 @@ const samplePosts = [
       lorem.generateWords(3),
     ],
     image: {
-      link: 'https://i.redd.it/u6xrjmzbrf321.png',
-      alt: 'A close up shot of a pigeon with glorious ruffles.',
+      link: "https://i.redd.it/u6xrjmzbrf321.png",
+      alt: "A close up shot of a pigeon with glorious ruffles.",
     },
   },
   {
     postId: 3,
-    title: 'A Lovely Pidgeon',
-    date: '16/10/20',
-    author: 'Mohammad Feather',
+    title: "A Lovely Pidgeon",
+    date: "16/10/20",
+    author: "Mohammad Feather",
     text: lorem.generateParagraphs(7),
     highlights: [
       lorem.generateWords(3),
@@ -71,10 +70,8 @@ const samplePosts = [
       lorem.generateWords(3),
     ],
     image: {
-      link:
-        'https://image.freepik.com/free-photo/pidgeon-waters-edge_19485-37038.jpg',
-      alt:
-        'A pigeon with a spectacular green plumage stares thoughtfully into the distance.',
+      link: "https://image.freepik.com/free-photo/pidgeon-waters-edge_19485-37038.jpg",
+      alt: "A pigeon with a spectacular green plumage stares thoughtfully into the distance.",
     },
   },
 ];
